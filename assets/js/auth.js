f0ecabde3aff88ff708f718c9ef7d5d006e13b76
Robert Le <robertle982@gmail.com>
2024-05-24 22:27:01 -0400

    adding bootstrap alerts

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,3 +1,10 @@
+function showAlert(div, type,msg){
+    var putInDiv = document.getElementById(div)
+    putInDiv.innerHTML ='<div class="alert alert-'+type+' alert-dismissible fade show" role="alert">'+msg+'<button type="button" class="close" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">&times;</span></button> </div>'
+}
+
+
+
 $('#sign_up_form').submit(function(e){
     e.preventDefault()
     var email = document.querySelector('#sign_up_email')
@@ -7,6 +14,6 @@ $('#sign_up_form').submit(function(e){
     if(pwd1 === pwd2){
         
     } else{
-        
+        showAlert('sign_up_alerts','warning','Hi! The passwords are not matching!!')
     }
 })
