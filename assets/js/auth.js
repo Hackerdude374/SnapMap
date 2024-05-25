f262471ea10f635ef33799a1e43e05c2b28db2c5
Robert Le <robertle982@gmail.com>
2024-05-24 22:16:20 -0400

    adding bootstrap alerts

---
+++ assets/js/auth.js
@@ -0,0 +1,12 @@
+$('#sign_up_form').submit(function(e){
+    e.preventDefault()
+    var email = document.querySelector('#sign_up_email')
+    var pwd1 = document.querySelector('#sign_up_pwd1')
+    var pwd2 = document.querySelector('#sign_up_pwd2')
+
+    if(pwd1 === pwd2){
+        
+    } else{
+        
+    }
+})
