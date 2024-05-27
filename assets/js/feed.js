30dce24540e1e1d97e78f105304f72ee614e7209
Robert Le <robertle982@gmail.com>
2024-05-27 07:25:00 -0400

    green camera button

---
+++ assets/js/feed.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById('capturedImg')
+
+
+function addimgtocanvas(e){
+    var reader = new FileReader()
+    reader.onload = function(event){
+        var img = new Image()
+        img.onload = function(){
+            var ctx = canvas.getContext('2d')
+            ctx.drawimage(img, 0,0,240,320)
+        }
+        img.src = event.target.result
+    }
+    reader.readAsDataURL(e.files[0])
+}
