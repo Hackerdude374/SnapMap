b2f5fa51ef501b229fb486dd503cebcd647b79be
Robert Le <robertle982@gmail.com>
2024-05-27 19:41:57 -0400

    working photos in location!!

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,6 +1,8 @@
+var map
+mapboxgl.accessToken = 'pk.eyJ1IjoiamltYm9qb25lczEyMyIsImEiOiJjbHdqemkxdXMxNjN1MmpsZWQyMnhpZWF4In0.xFj5mvD2KTm3K9VwF-mupw';
 function mapinit(){
-    mapboxgl.accessToken = 'pk.eyJ1IjoiamltYm9qb25lczEyMyIsImEiOiJjbHdqemkxdXMxNjN1MmpsZWQyMnhpZWF4In0.xFj5mvD2KTm3K9VwF-mupw';
-    var map = new mapboxgl.Map({
+    
+   map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11'
     });
@@ -15,20 +17,24 @@ function mapinit(){
       .then((data) => {
         var ids = Object.keys(data)
 
-        for(i= 0; i >ids.length; i++){
+        for(i= 0; i <ids.length; i++){
           // create the popup
-    const popup = new mapboxgl.Popup({ offset: 25 }).setText(
-      '<p> Status : ' + data[ids[i]].status + ' </p> <img src' + data[ids[i]].image + ' /> <p> Email : ' + data[ids[i]].image + ' /> <p>'
+  //   const popup = new mapboxgl.Popup({ offset: 25 }).setText(
+  //     '<p> Status : ' + data[ids[i]].status + ' </p> <img src' + data[ids[i]].image + ' /> <p> Email : ' + data[ids[i]].image + ' /> <p>'
 
-  );
+  // );
+
+  var popup = new mapboxgl.Popup({ offset: 25 }).setHTML(
+    '<p> Status : ' + data[ids[i]].status + ' </p> <img src=' + data[ids[i]].image + ' /> <p> Email : ' + data[ids[i]].email + ' </p>'
+    );
 
   // create DOM element for the marker
   const el = document.createElement('div');
   el.id = 'marker';
 
   // create the marker
-  new mapboxgl.Marker(el)
-      .setLngLat(monument)
+  new mapboxgl.Marker()
+      .setLngLat([data[ids[i]].location.longitude, data[ids[i]].location.latitude])
       .setPopup(popup) // sets a popup on this marker
       .addTo(map);
 
