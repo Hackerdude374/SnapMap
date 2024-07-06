a9d9ef86ffb1a7cdb876f72b45dd218c5bcb99cf
Robert Le <robertle982@gmail.com>
2024-07-05 21:16:12 -0400

    whatevs

--- newapp/src/components/CustomHead.tsx
+++ newapp/src/components/CustomHead.tsx
@@ -9,10 +9,10 @@ const CustomHead: React.FC = () => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>SnapMap</title>
       <link rel="stylesheet" href="/assets/css/styles.css" />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js"></script>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
-      <script src="https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" defer></script>
       <link href="https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.css" rel="stylesheet" />
+      <script src="https://api.mapbox.com/mapbox-gl-js/v2.9.1/mapbox-gl.js" defer></script>
       <link rel="manifest" href="/manifest.json" />
       <link
         rel="stylesheet"
--- newapp/src/components/Navbar.tsx
+++ newapp/src/components/Navbar.tsx
@@ -1,17 +1,20 @@
 // src/components/Navbar.tsx
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#"><i className="fa-solid fa-video"></i> SnapMap</a>
+      <Link href="/" className="navbar-brand">
+        <i className="fa-solid fa-video"></i> SnapMap
+      </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
+            <Link href="/about" className="nav-link">About</Link>
           </li>
           <li className="nav-item" id="logoutBtn" style={{ display: 'none' }}>
             <a className="nav-link" onClick={() => { /* logout function */ }}>Logout</a>
